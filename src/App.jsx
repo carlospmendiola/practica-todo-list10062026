b82e4c16778e1c8a0177c7e767ab2b4e26400497
carlospmendiola <carlos@carlospmendiola.com>
2026-06-10 13:28:15 +0200

    Enlazados los componentes al App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { TodoAdd } from './components/TodoAdd'
+import { TodoList } from './components/TodoList'
 
 function App() {
 
 
   return (
     <>
+      <header>
+        <p>Práctica TodoList</p>
+      </header>
+      <h1>Lista de Tareas</h1>
+      <TodoAdd/>
+      <TodoList/>
+     
     </>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,10 @@
+import { Todo } from "./Todo"
+
 export const TodoList = () => {
   return (
+    <>
     <h2>Tareas</h2>
+    <Todo/>
+    </>
   )
 }
